fba01892f0619a4e68640005dc14b9994d1725ca
Tadej Novak <tadej@tano.si>
2024-11-18 00:02:01 +0100

    Add ability to sort from oldest first

--- src/photo/db/index.ts
+++ src/photo/db/index.ts
@@ -16,7 +16,7 @@ const parameterizeForDb = (field: string) =>
   `REPLACE(REPLACE(REPLACE(LOWER(TRIM(${field})), ',', ''), '/', ''), ' ', '-')`;
 
 export type GetPhotosOptions = {
-  sortBy?: 'createdAt' | 'createdAtAsc' | 'takenAt' | 'priority'
+  sortBy?: 'createdAt' | 'createdAtAsc' | 'takenAt' | 'takenAtAsc' | 'priority'
   limit?: number
   offset?: number
   query?: string
@@ -142,6 +142,8 @@ export const getOrderByFromOptions = (options: GetPhotosOptions) => {
     return 'ORDER BY created_at ASC';
   case 'takenAt':
     return 'ORDER BY taken_at DESC';
+  case 'takenAtAsc':
+    return 'ORDER BY taken_at ASC';
   case 'priority':
     return 'ORDER BY priority_order ASC, taken_at DESC';
   }
