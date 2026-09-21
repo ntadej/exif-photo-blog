3ce73ae33a69454da28855545cd501fcafbe37aa
Sam Becker <sam@sambecker.com>
2026-09-21 13:55:19 -0500

    Refine button styles

--- src/admin/DeleteFormButton.tsx
+++ src/admin/DeleteFormButton.tsx
@@ -33,14 +33,7 @@ export default function DeleteFormButton (
     title="Delete"
     icon={<BiTrash size={16} />}
     spinnerColor="text"
-    className={clsx(
-      className,
-      'text-red-500! dark:text-red-600!',
-      'active:bg-red-100/50! dark:active:bg-red-950/50!',
-      'disabled:bg-red-100/50! dark:disabled:bg-red-950/50!',
-      'border-red-200! hover:border-red-300!',
-      'dark:border-red-900/75! dark:hover:border-red-900!',
-    )}
+    className={clsx('error', className)}
     onFormSubmit={onFormSubmit}
   />;
 }
